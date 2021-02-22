ee7035a2089abbaa84cce7651f73da938290835d
CodeCode231 <chrisyiangou97@outlook.com>
2021-02-22 16:10:21 +0000

    IP-24 #comment These are the other files I have created to start testing once I have sorted out issues with Domain

--- src/test/java/com/qa/ims/persistence/domain/CustomerTest.java
+++ src/test/java/com/qa/ims/persistence/domain/CustomerTest.java
@@ -1,11 +1,17 @@
 package com.qa.ims.persistence.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 
 public class CustomerTest {
 
+	@Before
+	public void beforeTest() {
+		System.out.println("COMMENCING TESTING");
+	}
+	
 	@Test
 	public void testEquals() {
 		EqualsVerifier.simple().forClass(Customer.class).verify();
